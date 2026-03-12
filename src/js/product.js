7f50cd70ab3a1631effd1bd83406d1875c5c7af7
Maritherecua <mrtrsarroyo@yahoo.com>
2026-03-11 21:37:48 -0700

    imported new function into product.js

--- src/js/product.js
+++ src/js/product.js
@@ -1,5 +1,6 @@
 import { setLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
+import { getQueryParam } from "./utils.mjs";
 
 const dataSource = new ProductData("tents");
 
