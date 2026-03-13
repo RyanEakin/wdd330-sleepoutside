30ef24910f63dc7d45cf533cc8d4958e8b4d3440
Ryan Eakin <RyanEakin@protonmail.com>
2026-03-13 17:02:45 -0600

    first-commit-URLParam-functionality

--- src/js/product.js
+++ src/js/product.js
@@ -1,7 +1,10 @@
-import { getLocalStorage, setLocalStorage } from "./utils.mjs";
+import { getLocalStorage, getParam, setLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
+const productId = getParam('product');
+
+//console.log(dataSource.findProductById(productId));
 
 function addProductToCart(product) {
   // Get stored cart, or set as empty array
