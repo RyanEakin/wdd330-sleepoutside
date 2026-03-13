d5a61184230823458b24ce8fd7f57c161c5ad194
Maritherecua <mrtrsarroyo@yahoo.com>
2026-03-13 16:39:32 -0700

    rebderList method to improve performance

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -3,6 +3,30 @@
 /*there are more than one category of products that will need to be independently listed. To make the ProductList class
 as flexible and reusable as possible, the constructor should receive the parameters: category, dataSource,
 and the HTML element (listElement) as an output target*/
+function productCardTemplate(product, productUrl = "#") {
+    return `
+      <li class="product-card">
+        <a href="${productUrl}">
+          <img src="${product.Image.replace("../", "/")}" alt="${product.NameWithoutBrand}" />
+          <h3 class="card__brand">${product.Brand?.Name || ""}</h3>
+          <h2 class="card__name">${product.NameWithoutBrand}</h2>
+          <p class="product-card__price">$${Number(product.FinalPrice).toFixed(2)}</p>
+        </a>
+      </li>
+    `;
+}
+
+const productPageMap = {
+    "880RR": "/product_pages/marmot-ajax-3.html",
+    "989CG": "/product_pages/northface-talus-4.html",
+    "985PR": "/product_pages/northface-alpine-3.html",
+    "344YJ": "/product_pages/cedar-ridge-rimrock-2.html",
+};
+
+function resolveProductUrl(product) {
+    return productPageMap[product.Id] || "#";
+}
+
 export default class ProductList {
     constructor(category, dataSource, listElement) {
         this.category = category;
@@ -11,28 +35,26 @@ export default class ProductList {
     }
     /*Next, add a method called renderList(). This method will be responsible for generating the HTML for the product cards
     and inserting them into the listElement */
-    async renderList() {
-        const products = await this.dataSource.getData();
-        const productPageMap = {
-            "880RR": "product_pages/marmot-ajax-3.html",
-            "989CG": "product_pages/northface-talus-4.html",
-            "985PR": "product_pages/northface-alpine-3.html",
-            "344YJ": "product_pages/cedar-ridge-rimrock-2.html",
-        };
+    async renderList(products = null, options = {}) {
+        const {
+            listElement = this.listElement,
+            template = productCardTemplate,
+            linkResolver = resolveProductUrl,
+            insertMode = "replace",
+        } = options;
 
-        this.listElement.innerHTML = products
-            .map(
-                (product) => `
-      <li class="product-card">
-        <a href="${productPageMap[product.Id] || "#"}">
-          <img src="${product.Image.replace("../", "/")}" alt="${product.NameWithoutBrand}" />
-          <h3 class="card__brand">${product.Brand?.Name || ""}</h3>
-          <h2 class="card__name">${product.NameWithoutBrand}</h2>
-          <p class="product-card__price">$${Number(product.FinalPrice).toFixed(2)}</p>
-        </a>
-      </li>
-    `
-            )
+        const items = products ?? await this.dataSource.getData();
+
+        const html = items
+            .map((product) => template(product, linkResolver(product)))
             .join("");
+
+        if (insertMode === "afterbegin") {
+            listElement.insertAdjacentHTML("afterbegin", html);
+        } else {
+            listElement.innerHTML = html;
+        }
+
+        return items;
     }
-}
+}   
