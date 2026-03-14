b48f12ea051de6db7d5027e074d6121f6c212b8b
meetlawrence <lokon4@byupathway.edu>
2026-03-14 17:03:40 +0100

    I completed cart total

--- src/js/cart.js
+++ src/js/cart.js
@@ -2,8 +2,28 @@ import { getLocalStorage } from "./utils.mjs";
 
 function renderCartContents() {
   const cartItems = getLocalStorage("so-cart");
-  const htmlItems = cartItems.map((item) => cartItemTemplate(item));
-  document.querySelector(".product-list").innerHTML = htmlItems.join("");
+
+  // Check if cartItems is null
+  if (cartItems && cartItems.length > 0) {
+    const htmlItems = cartItems.map((item) => cartItemTemplate(item));
+    document.querySelector(".product-list").innerHTML = htmlItems.join("");
+
+    // Show the footer and calculate total
+    document.querySelector(".cart-footer").classList.remove("hide");
+    calculateTotal(cartItems);
+  } else {
+    // If cart is empty, it will display a message or hide the footer
+    document.querySelector(".product-list").innerHTML = "<p>Your cart is empty.</p>";
+    document.querySelector(".cart-footer").classList.add("hide");
+  }
+}
+
+function calculateTotal(cartItems) {
+  // I use reduce to sum up the FinalPrice of all items
+  const total = cartItems.reduce((sum, item) => sum + item.FinalPrice, 0);
+  
+  // Update the HTML with the formatted total
+  document.querySelector(".cart-total").innerText = `Total: $${total.toFixed(2)}`;
 }
 
 function cartItemTemplate(item) {
