ae318e5a3727b9670987f1b753f132c4d9a6cd42
Maritherecua <mrtrsarroyo@yahoo.com>
2026-03-13 18:50:51 -0700

    updated file 'vite.config.js'

--- vite.config.js
+++ vite.config.js
@@ -11,22 +11,7 @@ export default defineConfig({
         main: resolve(__dirname, "src/index.html"),
         cart: resolve(__dirname, "src/cart/index.html"),
         checkout: resolve(__dirname, "src/checkout/index.html"),
-        productMarmot: resolve(
-          __dirname,
-          "src/product_pages/marmot-ajax-3.html",
-        ),
-        productNorthfaceAlpine: resolve(
-          __dirname,
-          "src/product_pages/northface-alpine-3.html",
-        ),
-        productNorthfaceTalus: resolve(
-          __dirname,
-          "src/product_pages/northface-talus-4.html",
-        ),
-        productCedarRidge: resolve(
-          __dirname,
-          "src/product_pages/cedar-ridge-rimrock-2.html",
-        ),
+        product: resolve( __dirname, "src/product_pages/index.html"),
       },
     },
   },
